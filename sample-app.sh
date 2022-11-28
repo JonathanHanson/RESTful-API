0abe414e651e8c5ad39dd4bce8d567062d3f7ae2
JonathanHanson <119131282+JonathanHanson@users.noreply.github.com>
2022-11-28 14:18:46 +1300

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -11,6 +11,7 @@ echo "RUN apt install pip" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  assessment_code.py /home/myapp/" >> tempdir/Dockerfile
 echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
+echo "RUN python3 /home/myapp/assessment_code.py" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/assessment_code.py" >> tempdir/Dockerfile
 
